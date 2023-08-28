888cd2b759050ee7af764ff11bf815c58ae15e68
jerife <jerife@naver.com>
2023-08-28 22:50:27 +0900

    feat: SNS기반 이미지 크롤러 기능

---
+++ trend_crawling/utils/__init__.py
@@ -0,0 +1,1 @@
+from .help_function import *
---
+++ trend_crawling/utils/help_function.py
@@ -0,0 +1,16 @@
+import os, json
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+secret_file = os.path.join(BASE_DIR, 'secrets.json')  # secrets.json 파일 위치를 명시
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise print(error_msg)
+    

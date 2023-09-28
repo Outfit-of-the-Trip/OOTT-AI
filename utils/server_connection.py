afb0204908f88478b1e33e13918180c492b89d0b
jerife <jerife@naver.com>
2023-09-28 23:04:06 +0900

    feat: Add DB 'insert' code  about coorid

--- utils/server_connection.py
+++ utils/server_connection.py
@@ -1,7 +1,7 @@
 import pymysql
 from datetime import datetime
 
-def transfer_data_to_db(crop_list, label_list, color_list, combi_list, categori_list, recommend_type, secret):
+def transfer_data_to_db(crop_list, label_list, color_list, categori_list, combi_list, combi_color_list, recommend_type, secret):
     conn = pymysql.connect(
         host=secret['server']['db']['host'],
         user=secret['server']['db']['user'], 
@@ -13,8 +13,10 @@ def transfer_data_to_db(crop_list, label_list, color_list, combi_list, categori_
     cur = conn.cursor()
     date = datetime.now().strftime("%Y-%m-%d")
     
-    for crop, label, color, combi, categori in zip(crop_list, label_list, color_list, combi_list, categori_list):
+    for label, color, categori in zip(label_list, color_list, categori_list):
         cur.execute(f"INSERT INTO CRAWL_DATA(crawlCategory, crawlClothesCategory, crawlClothes, crawlColor, crawlCount, crawlDate) VALUES ('{recommend_type}','{categori}','{label}','{color}',{1},'{date}')")
+    for combi, combi_color in zip(combi_list, combi_color_list):
+        cur.execute(f"INSERT INTO CRAWL_DATA(cooridiCategory, cooridiClothes, cooridiColor, cooridiCount, cooridiDate) VALUES ('{recommend_type}','{','.join(combi)}',{','.join(combi_color)}, {1}, '{date}')")
     
     conn.commit()
     conn.close()
